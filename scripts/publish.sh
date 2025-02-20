af158358e33cb175a57a84ad65f9194225e8fab1
kit101 <qkssk1711@163.com>
2025-02-20 11:19:06 +0800

    update scripts/publish.sh

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -8,18 +8,41 @@ tag="$1"
 ocifile=".oci.`date +%s`"
 
 
-clean="1"
+noclean="0"
 if printf "%s\n" "$@" | grep -q -- '--no-clean'; then
-  clean="0"
+  noclean="1"
+fi
+skopeo="0"
+if printf "%s\n" "$@" | grep -q -- '--skopeo'; then
+  skopeo="1"
+fi
+push="0"
+if printf "%s\n" "$@" | grep -q -- '--push'; then
+  push="1"
 fi
 
-set -ex
+run() {
+  local cmd="$1"
+  echo "+ $cmd"
+  eval "$cmd"
+  echo ""
+}
 
 
-docker buildx b -t $image:$tag --platform linux/amd64,linux/arm64 -o type=oci,dest=$ocifile,tar=false . --no-cache
+build_cmd="docker buildx b -t $image:$tag --platform linux/amd64,linux/arm64 --no-cache ."
 
-skopeo copy oci:$ocifile docker://$registry/$image:$tag --all
+#set -xe
 
-if [ "$clean" == "1" ]; then
-  rm -rf .oci.*
+if [ "$skopeo" == "1" ]; then
+  final_build_cmd=$( [ "$push" == "1" ] && echo "$build_cmd -o type=oci,dest=$ocifile,tar=false" || echo "$build_cmd" )
+  run "$final_build_cmd"
+  if [ "$push" == "1" ]; then
+    run "skopeo copy oci:$ocifile docker://$registry/$image:$tag --all"
+  fi
+  if [ "$noclean" == "0" ]; then
+    run "rm -rf .oci.*"
+  fi
+else
+  final_build_cmd=$( [ "$push" == "1" ] && echo "$build_cmd --push" || echo "$build_cmd" )
+  run "$final_build_cmd"
 fi
